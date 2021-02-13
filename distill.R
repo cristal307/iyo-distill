9dc249cc0d7ac6fb195945b1b83269df78b3102e
Cristal Rivera <riveracristal07@gmail.com>
2021-02-12 18:16:07 -0600

    changes

--- distill.R
+++ distill.R
@@ -2,3 +2,7 @@ library(distill)
 #library(postcards)
 
 create_website(dir = ".", title = "iyo-distill", gh_pages = TRUE)
+
+
+#POSTCARDS
+postcards::create_postcard(template = "jolla", file = "about.Rmd") 
